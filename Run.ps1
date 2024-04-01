fd8770094753921af8df325cc7ebb5c124585997
Mustafa Merchant <74061610+BlueStreak79@users.noreply.github.com>
2024-04-01 13:07:22 +0530

    Update Run.ps1

--- Run.ps1
+++ Run.ps1
@@ -11,10 +11,10 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v St
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 1 /f
 
 # Enable automatically pick an accent color from my background
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemAccentColorAuto /t REG_DWORD /d 1 /f
 
 # Enable title bars & window borders
-reg add "HKCU\Control Panel\Colors" /v TitleText /t REG_SZ /d "0 0 0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f
 
 # Enable desktop icons: Computer, User's Files, Network, Recycle Bin, Control Panel
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f
